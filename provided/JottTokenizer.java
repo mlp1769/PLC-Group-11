3242a7562f6309f4d3cf869fce3c2d6a73b0eb53
Mathew Prokuda <mlp1769@g.rit.edu>
2025-09-17 19:27:03 -0400

    Added tokenization for keywords.

--- provided/JottTokenizer.java
+++ provided/JottTokenizer.java
@@ -29,10 +29,29 @@ public class JottTokenizer {
 			boolean decimal = false;
 			boolean comment = false;
 			boolean incompleteToken = false;
+			boolean isKeyword = false;
+			String keywordBeingGenerated = "";
 
 			int line = 1;
 			while ((character = reader.read()) != -1) {
 				char c = (char) character;
+				if(Character.isWhitespace(c) || c==',' || c==']' || c=='[' || c=='}' || c=='{' || c=='}' || c=='=' || c=='<' || c=='>' || c=='/' || c=='+' || c=='-' || c=='*' || c==';' || c=='.' || c==':' || c=='!' || c=='"'){
+					if(isKeyword){
+						//turn keyword into token and make isKeyword false
+						tokens.add(new Token(keywordBeingGenerated, filename, line, TokenType.ID_KEYWORD));
+						keywordBeingGenerated="";
+						isKeyword = false;
+					}
+
+				}
+				if(isKeyword){
+					if(Character.isDigit(c) || Character.isAlphabetic(c)){
+						keywordBeingGenerated = (keywordBeingGenerated+c);
+					}else{
+						//TODO throw an error
+					}
+				}
+
 				if(c != '\n'){
 			        //Single charater tokens
 					if(c == ','){
@@ -96,10 +115,10 @@ public class JottTokenizer {
 						//If a number is in a string
 						if(isString){
 							result.append(c);
-						}else{
+						}else if(!isKeyword){
 							if(decimal && c == '.'){
 								System.err.println(String.format("Syntax Error Invalid \n token \"%c\" \"%c\" cannot follow \"%c\" in same token %s",c,c,c,filename));
-								return null;			
+								return null;
 							}
 							if (Character.isDigit(previousCharacter) || previousCharacter == '.') {
 								tokens.remove(tokens.size() - 1);
@@ -123,6 +142,12 @@ public class JottTokenizer {
 						if(isString){
 							result.append(c);
 						}
+						else if(!isKeyword){
+							//if(previousCharacter==0 || Character.isWhitespace(previousCharacter)){
+							isKeyword = true;
+							keywordBeingGenerated = ""+c;
+						}
+
 					}else if(c == '"'){
 						if(isString){
 							result.append(c);
@@ -156,6 +181,13 @@ public class JottTokenizer {
 				previousCharacter = c;
 			}
 			reader.close();
+			if(isKeyword){
+				//turn keyword into token and make isKeyword false
+				tokens.add(new Token(keywordBeingGenerated, filename, line, TokenType.ID_KEYWORD));
+				keywordBeingGenerated="";
+				isKeyword = false;
+
+			}
 			if(incompleteToken){
 				System.err.println(String.format("Syntax Error Invalid \n token \"%s\" \"%s\" is incomplete %s",result,result,filename));
 				return null;
