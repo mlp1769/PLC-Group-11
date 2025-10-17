f34eeb3c8dac8a281c9a17d0910b79f5ed97be14
ge7659 <ge7659@rit.edu>
2025-10-16 21:36:37 -0400

    Fixed ReturnStatementNode

--- parserclasses/ReturnStatementNode.java
+++ parserclasses/ReturnStatementNode.java
@@ -2,32 +2,38 @@ package parserclasses;
 
 import provided.JottTree;
 import provided.Token;
+import provided.TokenType;
 
 import java.util.ArrayList;
 
 public class ReturnStatementNode implements JottTree {
 
-    ExpressionNode expressionNode;
+    private ExpressionNode expressionNode;
 
     public ReturnStatementNode(ExpressionNode expressionNode) {
         this.expressionNode = expressionNode;
     }
 
     public static ReturnStatementNode parseReturnStatementNode(ArrayList<Token> tokens) throws Exception{
-        Token returnToken = tokenizer.remove(0);
+        Token returnToken = tokens.remove(0);
 
-        if(!returnToken.getToken().equals("Return")) {
+        if(!returnToken.getTokenType().equals(TokenType.ID_KEYWORD) || !returnToken.getToken().equals("Return")) {
             throw new Exception("Missing return statement");
         }
 
         ExpressionNode expressionNode = ExpressionNode.parseExpressionNode(tokens);
+
+        Token semicolonToken = tokens.remove(0);
+        if(!semicolonToken.getTokenType().equals(TokenType.SEMICOLON)) {
+            throw new Exception("Missing semicolon");
+        }
 
         return new ReturnStatementNode(expressionNode);
     }
 
     @Override
     public String convertToJott() {
-        return "";
+        return "Return " + this.expressionNode.convertToJott()+";" ;
     }
 
     @Override
