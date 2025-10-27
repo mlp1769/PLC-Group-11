f6906d9b0037a589c9888f4ae23278049d622639
Defenestrated02 <om.malkan@gmail.com>
2025-10-27 17:36:40 -0400

    symbol table done

--- parserclasses/SymbolTable.java
+++ parserclasses/SymbolTable.java
@@ -2,33 +2,44 @@ package parserclasses;
 
 import java.util.HashMap;
 
+import provided.Token;
+import provided.TokenType;
+
 public class SymbolTable {
-    private static HashMap functionTable;
-    private static HashMap varTable;
-    private static String scope;
+    private static HashMap<String, String> functionTable = new HashMap<>();
+    private static HashMap<String, HashMap<String, String>> varTable = new HashMap<>();
+    private static String scope = "";
 
-    public SymbolTable(){
-        this.functionTable = new HashMap<>();
-        this.varTable = new HashMap<>();
+    public SymbolTable(){}
+
+    public static void addFunction(Token name, String type) throws Exception{
+        if(functionTable.keySet().contains(name.getToken())){
+            System.err.println(String.format("Semantic Error: %n Duplicate function %s %n %s:%d%n",
+                    name.getToken(), name.getFilename(), name.getLineNum()));
+            throw new Exception();
+        }
+        functionTable.put(name.getToken(), type);
+        varTable.put(name.getToken(), new HashMap<String, String>());
     }
 
-    public void addFunction(String name, String type){
-
+    public static void addVar(Token name, String type) throws Exception{
+        if(varTable.get(scope).keySet().contains(name.getToken())){
+            System.err.println(String.format("Semantic Error: %n Duplicate variable %s %n %s:%d%n",
+                    name.getToken(), name.getFilename(), name.getLineNum()));
+            throw new Exception();
+        }
+        varTable.get(scope).put(name.getToken(), type);
     }
 
-    public void addVar(String name, String type){
-
-    }
-
-    public String getFunction(String name){
-        return "";
+    public static String getFunction(String name){
+        return functionTable.get(name);
     }
     
-    public String getVar(String name){
-        return "";
+    public static String getVar(String name){
+        return varTable.get(scope).get(name);
     }
 
-    public void changeScope(String name){
-        
+    public static void changeScope(String name){
+        scope = name;
     }
 }
